66c887569119f514b64cf40ff31e380c92802694
Rahma Farag <96622170+Rahma-Farag@users.noreply.github.com>
2024-07-11 17:59:49 +0300

    Update llm_prompt.py
    remove quantization part

--- llm_prompt.py
+++ llm_prompt.py
@@ -63,13 +63,13 @@ def get_answer(READER_LLM, RAG_PROMPT_TEMPLATE, docs, question):
 
 def get_llm(READER_MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"):
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-    model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME, quantization_config=bnb_config)
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.bfloat16,
+    # )
+    model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)#, quantization_config=bnb_config)
     tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
 
     READER_LLM = pipeline(
